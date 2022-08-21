e74dfed026c44dda4d7313dc549491c4387bac77
ahsan <muhammadahsanraza42@gmail.com>
2022-08-22 00:23:12 +0500

    edit

--- serve_image_api/views.py
+++ serve_image_api/views.py
@@ -1,7 +1,7 @@
 from http.client import HTTPResponse
 from django.shortcuts import render
-
+from django.http.response import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HTTPResponse( "Hello Heroku")
+    return HttpResponse( "Hello Heroku")
